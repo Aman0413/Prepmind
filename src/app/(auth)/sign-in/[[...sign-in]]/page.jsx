6c1ecdbc61ba236e6ecac214a3107d39733c6c1a
Aman Verma <aman@Amans-MacBook-Air.local>
2024-06-29 20:57:34 +0530

    Fix user signin form

--- src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -5,8 +5,8 @@ export default function Page() {
   return (
     <div className="font-[sans-serif]">
       <div className="min-h-screen flex flex-col items-center justify-center">
-        <div className="grid md:grid-cols-2 items-center gap-4 max-md:gap-8 max-w-6xl max-md:max-w-lg w-full p-4 m-4 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md">
-          <div className="md:max-w-md w-full p-3">
+        <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-4 max-md:gap-8 max-w-6xl h-screen max-md:max-w-lg w-full p-4 m-4 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md">
+          <div className="md:max-w-md w-full p-3  flex justify-center items-center">
             <SignIn />
           </div>
 
