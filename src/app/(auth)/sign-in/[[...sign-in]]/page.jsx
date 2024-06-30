807efd510519defd957330b57c604b469b1a41e7
Aman Verma <aman@Amans-MacBook-Air.local>
2024-06-30 21:48:42 +0530

    fix change of sign-in page

--- src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,24 +1,12 @@
 import { SignIn } from "@clerk/nextjs";
-import Image from "next/image";
 
 export default function Page() {
   return (
-    <div className="font-[sans-serif]">
-      <div className="min-h-screen flex flex-col items-center justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-4 max-md:gap-8 max-w-6xl h-screen max-md:max-w-lg w-full p-4 m-4 shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] rounded-md">
-          <div className="md:max-w-md w-full p-3  flex justify-center items-center">
-            <SignIn />
-          </div>
-
-          <div className="md:h-full bg-[#000842] rounded-xl lg:p-12 p-8 hidden md:block">
-            <Image
-              src="https://readymadeui.com/signin-image.webp"
-              className="w-full h-full object-contain"
-              alt="login-image"
-              height={500}
-              width={500}
-            />
-          </div>
+    <div className="min-h-screen bg-white py-6 flex flex-col justify-center sm:py-12">
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-[#6462d1] to-[#4745d2] shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <SignIn />
         </div>
       </div>
     </div>
